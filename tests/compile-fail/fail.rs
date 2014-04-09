afeb76908976cb3d295ca0d823522fe136849c70
Naomi Nitel <naominitel@gmail.com>
2014-04-09 18:17:32 +0200

    Add a test suite.

---
+++ tests/compile-fail/fail.rs
@@ -0,0 +1,24 @@
+#![feature(phase)]
+
+#[phase(syntax)]
+extern crate rustlex;
+
+#[path="../common/strreader.rs"]
+mod strreader;
+
+type Token = ();
+
+rustlex!(
+    "sizeof" => println!("Saw token sizeof") //~ ERROR unreachable pattern
+    //~^ NOTE included in another pattern
+    ['a'-'z']+ => println!("Saw an identifier")
+)
+
+fn main() {
+    let str = "foo ";
+    let inp = strreader::reader(str) as ~::std::io::Reader;
+    let mut lexer = Lexer::new(inp);
+
+    for tok in lexer {
+    }
+}
